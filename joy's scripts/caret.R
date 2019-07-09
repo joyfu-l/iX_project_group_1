1afe7508a6741ef5d3d30fcae08556a079f3b0de
Joy Fu <joylingfu@gmail.com>
2019-07-09 11:55:56 +0200

    not willing to commit

--- joy's scripts/caret.R
+++ joy's scripts/caret.R
@@ -3,4 +3,16 @@ library(skimr)
 library(RANN)
 library(tidyverse)
 
-view(teaching_training_data)
+df <- read.csv("data/raw/teaching_training_data.csv")
+view(df)
+
+
+set.seed(100)
+# Step 1: Get row numbers for the training data
+trainRowNumbers <- createDataPartition(df$, p=0.8, list=FALSE)
+
+# Step 2: Create the training  dataset
+trainData <- heart[trainRowNumbers,]
+
+# Step 3: Create the test dataset
+testData <- heart[-trainRowNumbers,]
